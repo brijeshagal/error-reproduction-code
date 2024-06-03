618d4ca1ffb62b95c709e28c94c52057b31581d6
brijeshagal <brijeshagal@gmail.com>
2024-06-03 15:05:48 +0530

    error: connect button balance doesn't update

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,39 @@
 "use client";
-
-import { ConnectorType } from "@/enums";
-import { writeContract } from "@/utils/contractInteractions";
-import { erc20Abi } from "viem";
-import { useAccount } from "wagmi";
+import { useAddRecentTransaction } from "@rainbow-me/rainbowkit";
+import { createPublicClient, http } from "viem";
+import { polygon } from "viem/chains";
+import { useAccount, useWalletClient } from "wagmi";
 
 export default function Home() {
   const { address, connector, chainId } = useAccount();
+  const res = useWalletClient();
+  const addRecentTxn = useAddRecentTransaction();
   return (
     <main>
       <button
         onClick={async () => {
           if (address && chainId) {
-            const res = await writeContract({
-              chainId,
-              contractAddress: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
-              abi: erc20Abi,
-              functionName: "transfer",
-              args: ["0xA22042a57270F95169a230b7e766265a67759eBF", 100],
-              userAddress: address,
-              connectorType: connector?.type as ConnectorType,
+            const result = await res?.data?.sendTransaction({
+              to: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+              value: BigInt(3 * 10 ** 17),
             });
+            const publicClient = createPublicClient({
+              chain: polygon,
+              transport: http(),
+            });
+            if (result) {
+              const receipt = await publicClient.getTransactionReceipt({
+                hash: result,
+              });
+              if (receipt.status == "success") {
+                console.log("success");
+                addRecentTxn({
+                  hash: receipt.transactionHash,
+                  description:
+                    "Sent 0.3 MATIC to 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9",
+                });
+              }
+            }
           }
         }}
       >
